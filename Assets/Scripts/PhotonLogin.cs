4b6f4c88b2efbace4aafa5e595b4b2723b68ed5d
Viktor.Vashchenkov <viktor.vashchenkov@arloid.com>
2022-01-21 19:41:06 +0300

    Lesson 3 HW

--- Assets/Scripts/PhotonLogin.cs
+++ Assets/Scripts/PhotonLogin.cs
@@ -1,9 +1,15 @@
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class PhotonLogin : MonoBehaviourPunCallbacks
 {
+    [SerializeField] private Button _photonLoginButton;
+    [SerializeField] private Button _photonLogoutButton;
+    [SerializeField] private Text _infoText;
+    
     private void Awake()
     {
         PhotonNetwork.AutomaticallySyncScene = true;
@@ -11,7 +17,8 @@ public class PhotonLogin : MonoBehaviourPunCallbacks
 
     private void Start()
     {
-        Connect();
+        _photonLoginButton.onClick.AddListener(Connect);
+        _photonLogoutButton.onClick.AddListener(Disconnect);
     }
 
     public void Connect()
@@ -26,9 +33,40 @@ public class PhotonLogin : MonoBehaviourPunCallbacks
         }
     }
 
+    public void Disconnect()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.Disconnect();
+        }
+        else
+        {
+            _infoText.text = "Photon: You are not connected";
+            _infoText.color = Color.yellow;
+        }
+    }
+
     public override void OnConnectedToMaster()
     {
         base.OnConnectedToMaster();
         Debug.Log("Photon Success");
+        _infoText.text = "Photon: success login!";
+        _infoText.color = Color.green;
+    }
+
+    public override void OnErrorInfo(ErrorInfo errorInfo)
+    {
+        base.OnErrorInfo(errorInfo);
+        Debug.Log("Photon Failed");
+        _infoText.text = "Photon: error login!\n" + errorInfo.Info;
+        _infoText.color = Color.red;
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        base.OnDisconnected(cause);
+        Debug.Log("Photon Disconnected");
+        _infoText.text = "Photon: disconnected";
+        _infoText.color = Color.green;
     }
 }
